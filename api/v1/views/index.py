e2939a9cf4c54b770821a1cfe9d4ed984f52a9b6
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-08 13:42:09 +0000

    Few changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Defines a Flask route"""
-from flask import jsonify, request
+from flask import jsonify, request, make_response
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """Returns a JSON response with the status OK"""
+    """
+    Returns a JSON response with the status OK
+    """
     response = {"status": "OK"}
     return jsonify(response)
 
@@ -15,12 +17,9 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each objects by type."""
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats)
+    stats_dict = {"Amenity": "amenities", "City": "cities",
+                  "Place": "places", "Review": "reviews",
+                  "State": "states", "User": "users"
+                  }
+    all_stats = {stats_dict[key]: storage.count(key) for key in stats_dict}
+    return jsonify(all_stats)
